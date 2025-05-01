245a882eeb9ce677787e6cc9e5ea10da9b20e30c
Marjan <max.xplodeart@gmail.com>
2025-05-01 18:56:13 +0200

    add polars to provision script

--- src/scripts/prepare.sh
+++ src/scripts/prepare.sh
@@ -4,7 +4,7 @@
 
 singularity build /d/hpc/home/ms88481/containers/container-marjan-cuda-torch.sif docker://pytorch/pytorch:2.7.0-cuda12.6-cudnn9-devel
 
-singularity exec /d/hpc/home/ms88481/containers/container-marjan-cuda-torch.sif pip install transformers datasets accelerate peft trl bitsandbytes protobuf blobfile sentencepiece
+singularity exec /d/hpc/home/ms88481/containers/container-marjan-cuda-torch.sif pip install transformers datasets accelerate peft trl bitsandbytes protobuf blobfile sentencepiece polars
 
 # singularity exec /d/hpc/home/ms88481/containers/container-marjan-cuda-torch.sif /d/hpc/home/ms88481/.local/bin/accelerate config
 # accelerate configuration saved at /d/hpc/projects/onj_fri/nlpmaxxing/hf/accelerate/default_config.yaml
