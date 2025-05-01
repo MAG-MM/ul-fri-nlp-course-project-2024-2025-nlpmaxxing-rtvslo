71de7d3196e807e08e8f3ffa18774728c1ade15c
SubicovGitHub <galsubic@gmail.com>
2025-05-01 16:47:21 +0200

    Evaluation with bertscore

---
+++ src/scripts/testing/evaluate_reports.py
@@ -0,0 +1,58 @@
+from bert_score import BERTScorer
+from typing import List, Dict
+
+
+class TrafficReportEvaluator:
+    def __init__(self, lang='sl', model='xlm-roberta-large'):
+        self.lang = lang
+        self.bertscore_model = model
+
+    def bert_scores(self, generated: List[str], references: List[str]) -> Dict[str, float]:
+        """
+        Compute BERTScore Precision, Recall, and F1 between two lists of texts.
+        """
+        scorer = BERTScorer(
+            lang=self.lang,
+            model_type=self.bertscore_model,
+            rescale_with_baseline=False     # gives not ideal scores for Slovenian
+        )
+        precision, recall, f1 = scorer.score(generated, references)
+        return {
+            'bertscore_precision': precision,
+            'bertscore_recall': recall,
+            'bertscore_f1': f1
+        }
+
+    def length_diff(self, generated: List[str], reference: List[str]) -> List[float]:
+        """
+        Compare lengths of generated reports.
+        """
+        lengths = []
+        for i in range(len(generated)):
+            lengths.append((len(generated[i].split()) - len(reference[i].split())) / max(len(reference[i].split()), 1))
+        return lengths
+
+
+if __name__ == "__main__":
+    generated = [
+        "Zaradi prometne nesreče je zaprta avtocesta A1 pri Celju.",
+        "Fakulteta za računalništvo in informatiko",
+        "Danes je lep dan, saj je lepo, sončno vreme.",
+        "Danes je lep dan, saj je lepo, sončno vreme.",
+        "Nek naključni testni niz.",
+        "ja"
+    ]
+    references = [
+        "Na avtocesti A1 pri Celju je zaradi nesreče zaprt promet.",
+        "Fakulteta za računalništvo ter informatiko",
+        "Na nebu sije sonce, kar je lepo.",
+        "Danes pada sneg in dež.",
+        "Komaj čakam na poletje, da se mi ni treba več s faksom ukvarjat.",
+        "ne"
+    ]
+    evaluator = TrafficReportEvaluator()
+    # evaluator = TrafficReportEvaluator(model='bert-base-multilingual-cased')
+    bertscore_results = evaluator.bert_scores(generated, references)
+    length_results = evaluator.length_diff(generated, references)
+    print(bertscore_results)
+    print(f"length diff: ", length_results)
